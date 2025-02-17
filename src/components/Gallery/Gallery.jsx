b3a4a4acf7e7e9f3a00792ef68f749845b929648
lukeyusuke <lukedev09@gmail.com>
2025-02-17 13:35:37 -0300

    style(gallery): add style to Gallery section

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,15 +1,17 @@
+import styles from './Gallery.module.css';
+
 const Gallery = () => {
     return (
         <>
-            <section className="gallery">
-                <div className="gallery__text">
-                    <h2>Nossa galeria está sempre aberta pra você</h2>
-                    <button className="btn">Ver na galeria</button>
-                </div>
-                <div className="gallery__images">
-                    <img src="./src/assets/images/gallery/lebron.png" alt="Lebron James - Jogador da NBA" />
-                    <img src="./src/assets/images/gallery/luka.png" alt="Luka Doncic - Jogador da NBA" />
-                    <img src="./src/assets/images/gallery/curry.png" alt="Stephen Curry - Jogador da NBA" />
+            <section id={styles.gallery}>
+                <div className={styles.galleryContent}>
+                    <div className={styles.galleryContent__text}>
+                        <h2>Nossa galeria está<br/> sempre aberta pra<br/> você</h2>
+                        <button className="btn">VER NA GALERIA</button>
+                    </div>
+                    <div className={styles.galleryContent__images}>
+                        <img src="./src/assets/images/Gallery/images.png" alt="Jogadores da NBA" />
+                    </div>
                 </div>
             </section>
         </>
