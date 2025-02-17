5ced6d37f7029651034c9b33268a13706b794250
lukeyusuke <lukedev09@gmail.com>
2025-02-17 16:32:00 -0300

    style(plays): add style to Plays section

--- src/components/Plays/Plays.jsx
+++ src/components/Plays/Plays.jsx
@@ -1,23 +1,26 @@
 import { BiChevronLeft } from "react-icons/bi";
 import { BiChevronRight } from "react-icons/bi";
+import styles from './Plays.module.css';
 
 const Plays = () => {
     return (
         <>
-            <section className="home">
-                <div className="home-main-text">
-                    <h1>JOGADAS</h1>
-                    <h2>JOGADAS INCRÍVEIS DA TEMPORADA</h2>
-                    <p>Os melhores jogadores, os maiores campeões e todas as histórias<br/> que já passaram pela quadra, chegou a hora da bola laranja subir!!  </p>
-                </div>
-                <div className="plays">
-                    <img src="./src/assets/images/plays/play2.png" alt="Vídeo da jogada 1"/>
-                    <img src="./src/assets/images/plays/play1.png" alt="Vídeo da jogada 2"/>
-                    <img src="./src/assets/images/plays/play2.png" alt="Vídeo da jogada 3"/>
-                </div>
-                <div className="buttons">
-                    <BiChevronLeft/>
-                    <BiChevronRight/>
+            <section id={styles.plays}>
+                <div className={styles.playsContent}>
+                    <div className={styles.playsContent__text}>
+                        <h1>JOGADAS</h1>
+                        <h2>JOGADAS INCRÍVEIS<br/> DA TEMPORADA</h2>
+                        <p>Os melhores jogadores, os maiores campeões e todas as histórias<br/> que já passaram pela quadra, chegou a hora da bola laranja subir!!  </p>
+                    </div>
+                    <div className={styles.playsContent__plays}>
+                        <img src="./src/assets/images/plays/play2.png" alt="Vídeo da jogada 1"/>
+                        <img src="./src/assets/images/plays/play1.png" alt="Vídeo da jogada 2"/>
+                        <img src="./src/assets/images/plays/play2.png" alt="Vídeo da jogada 3"/>
+                    </div>
+                    <div className={styles.buttons}>
+                        <BiChevronLeft className={styles.buttons__style}/>
+                        <BiChevronRight className={styles.buttons__style}/>
+                    </div>
                 </div>
             </section>
         </>
