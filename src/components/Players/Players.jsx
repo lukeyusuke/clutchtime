07f375ebecd5bf7f89388ba6a5fd3fe97c0977eb
lukeyusuke <lukedev09@gmail.com>
2025-02-17 14:40:31 -0300

    style(players): add style to Players section

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,23 +1,26 @@
 import PlayersCards from "./PlayersCards";
+import styles from './Players.module.css';
 
 const Players = () => {
     return (
         <>
-            <section className="players">
-                <div className="players-text">
-                    <h1>JOGADORES</h1>
-                    <h2>ALGUNS DOS MELHORES JOGADORES DO MUNDO</h2>
+            <section id={styles.players}>
+                <div className={styles.playersContent}>
+                    <div className={styles.playersContent__text}>
+                        <h1>JOGADORES</h1>
+                        <h2>ALGUNS DOS MELHORES JOGADORES DO MUNDO</h2>
+                    </div>
+                    <div className={styles.playersContent__box}>
+                        <PlayersCards image='./src/assets/images/players/shai.png' text='Shai Gilgeous-Alexander - Jogador da NBA' name='Shai Gilgeous' last_name='ALEXANDER' height='1.98' weight='88kg' position='Armador' dt_birth='12/07/1998' age='26' from='Toronto, Canadá'/>
+                        <PlayersCards image='./src/assets/images/players/antman.png' text='Anthony Edwards - Jogador da NBA' name='Anthony' last_name='EDWARDS' height='1.93' weight='102kg' position='Ala-armador' dt_birth='05/08/2001' age='23' from='Geórgia, EUA'/>
+                        <PlayersCards image='./src/assets/images/players/lamelo.png' text='LaMelo Ball - Jogador da NBA' name='LaMelo' last_name='BALL' height='2.01' weight='82kg' position='Armador' dt_birth='22/08/2001' age='23' from='Califórnia, EUA'/>
+                        <PlayersCards image='./src/assets/images/players/morant.png' text='Ja Morant - Jogador da NBA' name='Ja' last_name='MORANT' height='1.88' weight='79kg' position='Armador' dt_birth='10/08/1999' age='25' from='Carolina do Sul, EUA'/>
+                    </div>
                 </div>
-                <div className="players-box">
-                    <PlayersCards image='./src/assets/images/players/shai.png' text='Shai Gilgeous-Alexander - Jogador da NBA' name='Shai Gilgeous' last_name='ALEXANDER' height='1.98' weight='88kg' position='Armador' dt_birth='12/07/1998' age='26' from='Toronto, Canadá'/>
-                    <PlayersCards image='./src/assets/images/players/antman.png' text='Anthony Edwards - Jogador da NBA' name='Anthony' last_name='EDWARDS' height='1.93' weight='102kg' position='Ala-armador' dt_birth='05/08/2001' age='23' from='Geórgia, EUA'/>
-                    <PlayersCards image='./src/assets/images/players/lamelo.png' text='LaMelo Ball - Jogador da NBA' name='LaMelo' last_name='BALL' height='2.01' weight='82kg' position='Armador' dt_birth='22/08/2001' age='23' from='Califórnia, EUA'/>
-                    <PlayersCards image='./src/assets/images/players/morant.png' text='Ja Morant - Jogador da NBA' name='Ja' last_name='MORANT' height='1.88' weight='79kg' position='Armador' dt_birth='10/08/1999' age='25' from='Carolina do Sul, EUA'/>
-                </div>
-                <div className="carousel">
-                    <img src="./src/assets/images/lines/line.png" alt="Lista com nome de jogadores"/>
-                    <img src="./src/assets/images/lines/line2.png" alt="Lista com nome de jogadores"/>
-                </div>
+                    <div className={styles.carousel}>
+                        <img src="./src/assets/images/lines/line.png" alt="Lista com nome de jogadores"/>
+                        <img src="./src/assets/images/lines/line2.png" alt="Lista com nome de jogadores"/>
+                    </div>
             </section>
         </>
     )   
--- src/components/Players/PlayersCards.jsx
+++ src/components/Players/PlayersCards.jsx
@@ -1,24 +1,29 @@
+import styles from './Players.module.css';
+
 const PlayersCards = ({image, text, name, last_name ,height, weight, position, dt_birth, age, from}) => {
     return (
         <>
-            <img src={image} alt={text}/>
-            <div className="players-box__info">
-                <h2>{name}<br/>{last_name}</h2>
-                <div className="players-box__info__hw">
-                    <div className="players-box__info__height">
-                        <p> Altura </p>
-                        <span>{height}</span>
+            <div className={styles.playersContent__stats}>
+                <img src={image} alt={text}/>
+                <div className={styles.playersContent__info}>
+                    <h3>{name}</h3>
+                    <h2>{last_name}</h2>
+                    <div className={styles.playersContent__info__hw}>
+                        <div className={styles.playersContent__height}>
+                            <p> Altura </p>
+                            <span>{height}</span>
+                        </div>
+                        <div className={styles.playersContent__weight}>
+                            <p> Peso </p>
+                            <span>{weight}</span>
+                        </div>
                     </div>
-                    <div className="players-box__info__weight">
-                        <p> Peso </p>
-                        <span>{weight}</span>
+                    <div className={styles.playersContent__info__stats}>
+                        <p> Posição: {position}</p>
+                        <p> Nasceu em: {dt_birth}</p>
+                        <p> Idade: {age} anos</p>
+                        <p> De: {from}</p>
                     </div>
-                </div>
-                <div className="players-box__info__stats">
-                    <p> Posição: {position}</p>
-                    <p> Nasceu em: {dt_birth}</p>
-                    <p> Idade: {age}</p>
-                    <p> De: {from}</p>
                 </div>
             </div>
         </>
